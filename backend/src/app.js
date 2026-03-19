92e6c8fc1704b751e0a4c4d71df96bcc008167ff
KunalChoudhary03 <kunalchoudhary3344@gmail.com>
2026-03-19 11:26:45 +0530

    Fixing

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,3 +20,9 @@ app.use("/api/admin", adminRoutes)
 app.use("/api/split", splitRoutes)
 app.use("/api/room", roomRoutes)
 module.exports = app
+
+
+// create leader borad for rooms in which user can see their register user can see others room pattern and can join the room and can see the pattern of other user's expense and can judge by them,  by there exepenses this  can if the user wants to join there room they can join in real room . 
+
+// user profile mai users k bare mai or unka opinion ki vo kya jayada spend karte hai ya kam aur unka pattern kya hai aur uske hisab se unko suggestions dena ki unka pattern kaisa hai aur unko kya karna chahiye taki unka pattern acha ho sake.
+// room ki location or request send karna or bhi jo yad ho teko vo bana dena 
